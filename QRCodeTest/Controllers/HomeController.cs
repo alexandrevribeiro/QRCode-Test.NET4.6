4a3f2cb33f89986e9cc1d76c1edc1083d476df0a
Alexandre Ribeiro <vribeiro.alexandre@gmail.com>
2017-06-01 01:26:11 -0300

    New MVC project using .NET 4.6

---
+++ QRCodeTest/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using QRCoder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QRCode_Test.NET4._6.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return RedirectToAction("QRCode");
+        }
+
+        public ActionResult Home()
+        {
+            return View("Index");
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult QRCode(string text = null)
+        {
+            text = text ?? Guid.NewGuid().ToString();
+
+            using (QRCodeGenerator qrGenerator = new QRCodeGenerator())
+            {
+                using (QRCodeData qrCodeData = qrGenerator.CreateQrCode(text, QRCodeGenerator.ECCLevel.Q))
+                {
+                    using (var qrCode = new Base64QRCode(qrCodeData))
+                    {
+                        ViewBag.ImageAsBase64 = qrCode.GetGraphic(20);
+                    }
+                }
+            }
+            
+            return View();
+        }
+    }
+}
---
+++ QRCodeTest/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="QRCode_Test.NET4._6.MvcApplication" Language="C#" %>
